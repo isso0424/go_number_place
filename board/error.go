41a640063988beed4f470025da8140ed015171aa
isso0424 <isso04240129@gmail.com>
2021-08-25 02:57:28 +0900

    feat: board input error

---
+++ board/error.go
@@ -0,0 +1,23 @@
+package board
+
+import (
+	"fmt"
+	"isso0424/go_number_place/coordinate"
+)
+
+type BoardError interface {
+	error
+	errorCode() int
+}
+
+type AlreadyInputted struct {
+	coor coordinate.Coordinate
+}
+
+func(e AlreadyInputted) Error() string {
+	return fmt.Sprintf("Coordinate (%d %d) is already inputted.", e.coor.GetX(), e.coor.GetY())
+}
+
+func(e AlreadyInputted) errorCode() int {
+	return 0
+}
