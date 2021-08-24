456806deedaf4d0cf05912ea7b7d9b180187a82b
isso0424 <isso04240129@gmail.com>
2021-08-25 01:47:37 +0900

    feat: define ValueNotFoundError

--- coordinate/error.go
+++ coordinate/error.go
@@ -2,7 +2,21 @@ package coordinate
 
 import "fmt"
 
+type ErrorCode int
+
 type CoordinateError interface {
 	error
-	GetErrorCode() int
+	GetErrorCode() ErrorCode
+}
+
+type ValueNotFound struct {
+	value int
+}
+
+func(e ValueNotFound) Error() string {
+	return fmt.Sprintf("%d is not found", e.value)
+}
+
+func(e ValueNotFound) GetErrorCode() ErrorCode {
+	return 0
 }
