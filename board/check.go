e15e51f2ad4fd1183a4f6b52ed75656cac6cb34c
isso0424 <isso04240129@gmail.com>
2021-08-25 02:43:34 +0900

    feat: check consist function

--- board/check.go
+++ board/check.go
@@ -2,6 +2,49 @@ package board
 
 import "isso0424/go_number_place/coordinate"
 
+func(b *Board) checkConsistNumberPlace() bool {
+	for _, arr := range(b.numbers) {
+		if !checkAllValueDiffirence(arr) {
+			return false
+		}
+	}
+
+	tmpPlane := [][]int{{}, {}, {}, {}, {}, {}, {}, {}, {}}
+	for _, arr := range(b.numbers) {
+		for x, v := range(arr) {
+			tmpPlane[x] = append(tmpPlane[x], v)
+		}
+	}
+
+	for _, arr := range(tmpPlane) {
+		if !checkAllValueDiffirence(arr) {
+			return false
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			tmp := []int{
+				b.getNumber(coordinate.New(i * 3 + 0, j * 3 + 0)),
+				b.getNumber(coordinate.New(i * 3 + 1, j * 3 + 0)),
+				b.getNumber(coordinate.New(i * 3 + 2, j * 3 + 0)),
+				b.getNumber(coordinate.New(i * 3 + 0, j * 3 + 1)),
+				b.getNumber(coordinate.New(i * 3 + 1, j * 3 + 1)),
+				b.getNumber(coordinate.New(i * 3 + 2, j * 3 + 1)),
+				b.getNumber(coordinate.New(i * 3 + 0, j * 3 + 2)),
+				b.getNumber(coordinate.New(i * 3 + 1, j * 3 + 2)),
+				b.getNumber(coordinate.New(i * 3 + 2, j * 3 + 2)),
+			}
+
+			if !checkAllValueDiffirence(tmp) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func checkAllValueDiffirence(arr []int) bool {
 	exist := make([]int, 9)
 	for  v := range(arr) {
