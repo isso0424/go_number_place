c887205bb44be1943158cb7f1c3b40d78d43fd6e
isso0424 <isso04240129@gmail.com>
2021-08-25 19:23:14 +0900

    feat: create input method

--- mock/input.go
+++ mock/input.go
@@ -8,3 +8,14 @@ type MockInputter struct {
 func NewMockInputter(results []string) MockInputter {
 	return MockInputter{ results: results, index: 0 }
 }
+
+func(inputter *MockInputter) Input() string {
+	if len(inputter.results) == inputter.index {
+		panic("Results over flow")
+	}
+
+	result := inputter.results[inputter.index]
+	inputter.index++;
+
+	return result
+}
