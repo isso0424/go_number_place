2a99536140de4726633256a0ea85ac8e123a0b8c
isso0424 <isso04240129@gmail.com>
2021-08-27 17:42:13 +0900

    feat: start point

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"isso0424/go_number_place/console"
+	"isso0424/go_number_place/generator"
+	"isso0424/go_number_place/system"
+)
 
 func main() {
-	fmt.Println("this is template")
+	gen := generator.EasyGenerator{}
+	problem, _ := gen.Generate()
+	inputter := console.NewInputter()
+	outputter := console.NewOutputter()
+	sys := system.New(problem, &inputter, &outputter)
+	sys.Start()
 }
